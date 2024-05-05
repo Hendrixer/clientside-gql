5b43ed5c7a1867b219cbdde2a16aef7c545966b0
Scott Moss <willscottmoss@gmail.com>
2024-05-05 17:26:26 -0500

    remove provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
-import GQLProvider from './gqlProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <GQLProvider>{children}</GQLProvider>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   )
